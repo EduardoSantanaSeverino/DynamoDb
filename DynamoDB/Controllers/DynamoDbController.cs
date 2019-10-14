0e979772b04ea1d47ad6908ddc964d537afbeb41
Eduardo Santana <eduardo_amparo@hotmail.com>
2019-10-14 00:37:44 -0400

    Adding files to make it work without HTTPs

---
+++ DynamoDB/Controllers/DynamoDbController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DynamoDb.Libs.DynamoDb;   
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DynamoDbController : Controller
+    {
+        private readonly IDynamoDbExamples _dynamoDbExample;
+
+        public DynamoDbController(IDynamoDbExamples dynamoDbExample)
+        {
+            _dynamoDbExample = dynamoDbExample;
+        }
+
+        [Route("createtable")]
+        public IActionResult CreateDynamoDbTable()
+        {
+            _dynamoDbExample.CreateDynamoDbTable();
+            return Ok();
+        }
+    }
+}
--- DynamoDB/Startup.cs
+++ DynamoDB/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using DynamoDb.Libs.DynamoDb;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,6 +35,17 @@ namespace DynamoDB
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+
+            Environment.SetEnvironmentVariable("AWS_ACCESS_KEY_ID", Configuration["AWS:AccessKey"]);
+            Environment.SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", Configuration["AWS:SecretKey"]);
+            Environment.SetEnvironmentVariable("AWS_REGION", Configuration["AWS:Region"]);
+
+            services.AddAWSService<IAmazonDynamoDB>();
+
+            services.AddSingleton<IDynamoDbExamples, DynamoDbExamples>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,11 +58,8 @@ namespace DynamoDB
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
